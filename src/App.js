d2f42110ebeef5b39aedcd1860e8d2a169a3f5e1
erik santiago <blueowl2794@gmail.com>
2022-03-01 10:02:02 -0500

    chanfle

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
-// import { Route, Switch } from 'react-router-dom';
+// import { Route, Switch } from 'react-router';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 import Landing from './components/Landing';
@@ -12,15 +12,12 @@ import DetailMenu from './components/DetailMenu';
 function App() {
   return (
     <div className="App">
-      
-      {/* <Footer/> */}
-
       <Routes>
-        {/* <Route exact path="/" element={<Landing/>} /> */}
-        {/* <Route path="/" element={ <Steps/>  } /> */}
-        {/* <Route path="/" element={ <Menu/> } /> */}
-        {/* <Route path="/" element={ <Contact/>} /> */}
-        {/* <Route path="/" element={ <Footer/> } /> */}
+        {/* <Route path="/" element={ <Landing/>} />
+        <Route path="/" element={ <Steps/>  } />
+        <Route path="/" element={ <Menu/> } />
+        <Route path="/" element={ <Contact/>} />
+        <Route path="/" element={ <Footer/> } /> */}
         <Route path="/" element={<> <Landing/> <Steps/> <Menu/> <Contact/> <Footer/> </>} />
         <Route path="/products/:id" element={<DetailMenu/>} />
       </Routes>
